e1b0539b67eec2a9d921827a3ae19649e1dfd2bb
DMZTdhruv <shruvparmar.gmail.com>
2024-09-13 04:50:25 +0530

    fixed all the resposnive bugs

--- src/section/markup_section/markup.tsx
+++ src/section/markup_section/markup.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 const MarkUpSection = () => {
   return (
     <section className="relative w-full  overflow-hidden">
-      <div className="relative max-w-7xl xl:h-[2000px] md:h-[1500px] sm:h-[1200px] xl:block flex flex-col items-center justify-center   mx-auto responsive_height xl:p-0 lg:px-[50px]">
+      <div className="relative max-w-7xl xl:h-[1500px] md:h-[1500px] sm:h-[1200px] xl:block flex flex-col items-center justify-center   mx-auto responsive_height xl:p-0 lg:px-[50px]">
         {/* markup section intro */}
-        <div className="text-center xl:mt-[600px] xl:text-left lg:pt-[153px] ">
+        <div className="text-center xl:mt-[200px] xl:text-left lg:pt-[153px] ">
           <h1 className="font-black leading-[100%] md:text-[100px] text-[70px] lg:text-[133px]">
           markup <br className="block lg:hidden" />
           </h1>
 
-          <p className="w-full xl:text-2xl text-xl xl:w-[480px] mt-[10px] px-[16px] sm:px-[100px] lg:px-0 pt-[20px]  text-body-text-secondary md:text-[18px] leading-[28px] lg:leading-[35px]">
+          <p className="w-full xl:text-2xl text-sm xl:w-[480px] mt-[10px] px-[16px] sm:px-[100px] lg:px-0 pt-[20px]  text-body-text-secondary md:text-[18px] leading-relaxed lg:leading-relaxed">
           When ready for deployment, a static-site generator such as Astro or Next.js is used to compile the website. The end result is a collection of pre-rendered HTML pages that can be delivered lightning-fast over a CDN like Vercel’s Edge Network.
           </p>
         </div>
@@ -22,7 +22,7 @@ const MarkUpSection = () => {
 
         <div
           draggable={false}
-          className="cards absolute flex gap-[46px] xl:top-[50%] top-[50%]  -translate-y-1/2 left-[50%] -translate-x-1/2 xl:left-auto xl:-translate-x-0 xl:right-[50px] xl:pt-[153px] scale-[0.4] md:scale-75 xl:scale-100 z-[-1]"
+          className="cards absolute flex gap-[46px] xl:top-[40%] top-[50%]  -translate-y-1/2 left-[50%] -translate-x-1/2 xl:left-auto xl:-translate-x-0 xl:right-[50px] xl:pt-[0px] scale-[0.4] md:scale-75 xl:scale-100 z-[-1]"
         >
           <div className="relative w-[280px] h-[629px] mt-[193px]">
             <Image
--- src/section/scores_section/scoreSection.tsx
+++ src/section/scores_section/scoreSection.tsx
@@ -12,7 +12,7 @@ export default function Component() {
   ];
 
   return (
-    <section className="relative bg-[#151515] h-[1100px] md:h-[1500px] lg:h-[2000px]">
+    <section className="relative bg-[#151515] h-[1100px] sm:h-[1300px] md:h-[1500px] lg:h-[2000px]">
       {/* card */}
       <div className="w-[90vw] sm:w-[80vw] md:w-[90vw] lg:w-[1024px] xl:w-[1204px] mx-auto z-10 gradient-border flex flex-col items-center text-white translate-y-[-100px] backdrop-blur-2xl border border-[#E8E8E8]/20 rounded-2xl overflow-hidden">
         <h2 className="text-4xl pb-8 sm:pb-12 md:pb-16 lg:pb-[95px] sm:text-5xl md:text-6xl lg:text-[70px] xl:text-[76px] text-center mt-8 sm:mt-12 md:mt-16 lg:mt-[120px] font-black leading-[100%]">
