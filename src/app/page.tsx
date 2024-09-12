80ce47c69c8737420b2258a8e7da79f5324c68a8
DMZTdhruv <shruvparmar.gmail.com>
2024-09-13 03:18:40 +0530

    Only rendering the failling icons with ssr:false now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,11 @@
 import dynamic from "next/dynamic";
 import HeroSection from "@/section/hero_section/heroSection";
-const JavascriptSection = dynamic(() => import("@/section/javascript_section/javascriptSection"), {
-  ssr: false,
-});
+const JavascriptSection = dynamic(() => import("@/section/javascript_section/javascriptSection"));
 const FallingIcons = dynamic(() => import("@/section/fallingIcons_section/fallingIcon"), {
   ssr: false,
 });
-const MarkUpSection = dynamic(() => import("@/section/markup_section/markup"), {
-  ssr: false,
-});
-const ScoreSection = dynamic(() => import("@/section/scores_section/scoreSection"), {
-  ssr: false,
-});
+const MarkUpSection = dynamic(() => import("@/section/markup_section/markup"));
+const ScoreSection = dynamic(() => import("@/section/scores_section/scoreSection"));
 
 export default function Home() {
   return (
